6998aa06821bc8664d369b01df5a23dfd0443e13
Pavithra-star <pavithra.resoju@ojas-it.com>
2021-05-06 21:36:40 +0530

    This is my POC-4 Task

---
+++ Spring_Boot_Poc4/src/main/java/com/example/SpringBootPoc4Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPoc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPoc4Application.class, args);
+	}
+
+}
